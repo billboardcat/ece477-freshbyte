d0e3ac4f5b36a7f2a9c1e4ecf1243dd7e10d55ed
Jimmy Sung <billboardcat@gmail.com>
2021-02-14 17:35:45 -0500

    Started work on the temperature and humidity sensor.
    We should make a separate .c/.h files for the HTS221

---
+++ ece477-hts221demo/Core/Inc/hts221.h
@@ -0,0 +1,24 @@
+/*
+ * hts221.h
+ *
+ *  Created on: Feb 14, 2021
+ *      Author: jimmysung
+ */
+
+#ifndef INC_HTS221_H_
+#define INC_HTS221_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32l0xx_hal.h"
+
+/* === REGISTER ADDRESSES ===  */
+#define HTS_WHO_AM_I 0x0F
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_HTS221_H_ */
