aa16613cd1957de0bc619a3999d07c85afe30e51
parky190 <pcrain@purdue.edu>
2021-02-23 20:40:10 -0500

    Added battery demo
    Still need to properly convert values to something usable...

---
+++ ece477-hts221-bq27441/Core/Inc/hts221.h
@@ -0,0 +1,89 @@
+/*
+ * hts221.h
+ *
+ *  Created on: Feb 14, 2021
+ *      Author: jimmysung + pcrain
+ */
+
+#ifndef INC_HTS221_H_
+#define INC_HTS221_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32l0xx_hal.h"
+
+/* === I2C ADDRESS == */
+#define HTS_ADDR 0x5F
+
+/* === REGISTER ADDRESSES ===  */
+#define HTS_WHO_AM_I 0x0F
+#define HTS_AV_CONF 0x10
+#define HTS_CTRL_REG1 0x20
+#define HTS_CTRL_REG2 0x21
+#define HTS_CTRL_REG3 0x22
+#define HTS_STATUS_REG 0x27
+#define HTS_HUMIDITY_OUT_L 0x28
+#define HTS_HUMIDITY_OUT_H 0x29
+#define HTS_TEMP_OUT_L 0x2A
+#define HTS_TEMP_OUT_H 0x2B
+#define HTS_CAL_H0_rH_x2 0x30
+#define HTS_CAL_H1_rH_x2 0x31
+#define HTS_CAL_T0_degC_x8 0x32
+#define HTS_CAL_T1_degC_x8 0x33
+#define HTS_CAL_T1_T0_msb 0x35
+
+#define HTS_CAL_H0_T0_OUT_L 0x36
+#define HTS_CAL_H0_T0_OUT_H 0x37
+
+#define HTS_CAL_H1_T0_OUT_L 0x3A
+#define HTS_CAL_H1_T0_OUT_H 0x3B
+
+#define HTS_CAL_T0_OUT_L 0x3C
+#define HTS_CAL_T0_OUT_H 0x3D
+
+#define HTS_CAL_T1_OUT_L 0x3E
+#define HTS_CAL_T1_OUT_H 0x3F
+
+/* === Error Return Values === */
+#define HUMID_ERROR -1
+#define TEMP_ERROR -460 //this is 1 deg. F below absolute 0 in F. in C absolute 0 is -273
+
+/* === REGISTER ADDRESS BITS === */
+// Let's just add the ones that we'll actually be using to keep it clean
+#define HTS_CTRL_REG1_PD 		(0x80)
+#define HTS_CTRL_REG1_BUD		(0x1 << 2)
+#define HTS_CTRL_REG2_ONE_SHOT	0x1
+
+/* === DATA STRUCTS === */
+typedef struct HTS_Cal{
+	//TEMP
+	int T0_OUT;
+	float temp_correction_factor;  //slope
+	//= (delta of (T1_degC_x8 w/ MSB / 8) - T0 of the same) / (T1_OUT - T0_OUT)
+	int temp_offset; //T0_degC_x8 w/ MSB then divided by 8
+	//zeroed_temp = T_out - T0_Out
+	//temp_adj = (zeroed_temp * correction_factor) + offset
+
+	//HUMIDITY
+	int H0_OUT;
+	float humid_correction_factor;
+	int humid_offset;
+
+} HTS_Cal;
+
+/* === FUNCTION DECLARATIONS === */
+HTS_Cal * hts221_init(void);
+int hts221_get_temp(char unit, HTS_Cal * hts_cal_data);
+int hts221_calc_temp(int16_t T_OUT, HTS_Cal * hts_cal_data);
+
+int hts221_get_humid(HTS_Cal * hts_cal_data);
+int hts221_calc_humid(int16_t H_OUT, HTS_Cal * hts_cal_data);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_HTS221_H_ */
---
+++ ece477-hts221-bq27441/Core/Src/stm32l0xx_it.c
@@ -0,0 +1,234 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32l0xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32l0xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "hts221.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim6;
+/* USER CODE BEGIN EV */
+extern HTS_Cal * hts_cal_data;
+extern int bq_init_ret;
+
+// TODO - change from enum to define
+typedef enum {
+	AVG,  // Average Current (DEFAULT)
+	STBY, // Standby Current
+	MAX   // Max Current
+} current_measure;
+
+// Parameters for the capacity() function, to specify which capacity to read
+typedef enum {
+	REMAIN,     // Remaining Capacity (DEFAULT)
+	FULL,       // Full Capacity
+	AVAIL,      // Available Capacity
+	AVAIL_FULL, // Full Available Capacity
+	REMAIN_F,   // Remaining Capacity Filtered
+	REMAIN_UF,  // Remaining Capacity Unfiltered
+	FULL_F,     // Full Capacity Filtered
+	FULL_UF,    // Full Capacity Unfiltered
+	DESIGN      // Design Capacity
+} capacity_measure;
+
+// Parameters for the soc() function
+typedef enum {
+	FILTERED,  // State of Charge Filtered (DEFAULT)
+	UNFILTERED // State of Charge Unfiltered
+} soc_measure;
+
+// Parameters for the soh() function
+typedef enum {
+	PERCENT,  // State of Health Percentage (DEFAULT)
+	SOH_STAT  // State of Health Status Bits
+} soh_measure;
+
+// Parameters for the temperature() function
+typedef enum {
+	BATTERY,      // Battery Temperature (DEFAULT)
+	INTERNAL_TEMP // Internal IC Temperature
+} temp_measure;
+
+// Parameters for the setGPOUTFunction() funciton
+typedef enum {
+	SOC_INT, // Set GPOUT to SOC_INT functionality
+	BAT_LOW  // Set GPOUT to BAT_LOW functionality
+} gpout_function;
+
+
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M0+ Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+  while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVC_IRQn 0 */
+
+  /* USER CODE END SVC_IRQn 0 */
+  /* USER CODE BEGIN SVC_IRQn 1 */
+
+  /* USER CODE END SVC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32L0xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32l0xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles TIM6 global interrupt and DAC1/DAC2 underrun error interrupts.
+  */
+void TIM6_DAC_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+  /* USER CODE END TIM6_DAC_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim6);
+  /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+  if (hts_cal_data != NULL){
+	  int temp = hts221_get_temp('C', hts_cal_data);
+	  if (temp == TEMP_ERROR) printf("Error reading temperature\r\n");
+	  else printf("Current temperature is %d C\r\n", temp);
+
+	  int humid = hts221_get_humid(hts_cal_data);
+	  if (humid == HUMID_ERROR) printf("Error reading humidity\r\n");
+	  else printf("Current Relative Humidity is %d%% \r\n", humid);
+
+	  uint16_t battery = BQ27441_voltage();
+//	  TODO: Stringify all these :'(
+//	  printf("Battery Voltage is ");
+
+	  uint16_t soc = BQ27441_soc(FILTERED);
+
+	  uint16_t current = BQ27441_current(AVG);
+	  uint16_t cap_remaining = BQ27441_capacity(REMAIN);
+	  uint16_t cap_max = BQ27441_capacity(FULL);
+	  int16_t power = BQ27441_power(); //average draw
+	  uint16_t soh = BQ27441_soh(PERCENT);
+	  uint16_t temp_bat = BQ27441_temperature(BATTERY);
+	  uint16_t temp_bq_IC = BQ27441_temperature(INTERNAL_TEMP);
+
+  }
+
+  /* USER CODE END TIM6_DAC_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
