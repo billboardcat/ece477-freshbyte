b50538fbd645e1bb89b0a4ebbd97cc1fa3ba1823
Jimmy Sung <billboardcat@gmail.com>
2021-03-23 19:59:30 -0400

    Wi-Fi Interfacing!
    Started work on wi-fi interfacing. Currently able to sent AT and have ESP8266 respond correctly.

---
+++ ece477-esp8266/Core/Inc/serial_print.h
@@ -0,0 +1,16 @@
+#ifndef __SERIAL_PRINT_H__
+#define __SERIAL_PRINT_H__
+
+enum uart_line_t {
+    WIFI,
+    DEBUG_PRINT
+};
+
+void serial_select(enum uart_line_t);
+void serial_clear(void);
+void serial_putc(char c);
+void serial_print(char string[]);
+void serial_println(char *string);
+void serial_printf(char format[], ...);
+
+#endif
